b73aeea8bb544c210987271a964c67ff44c33462
keptan <keptan@protonmail.com>
2019-02-06 14:25:27 -1000

    option to generate a shortened hash

--- serialPoll.js
+++ serialPoll.js
@@ -10,3 +10,24 @@ exports.hardwareSerial = function ()
 	}
 	catch(e) { return undefined};
 }
+
+exports.shortSerial = function ()
+{
+	const hash = function (str)
+	{
+		var acc = 0;
+		for(const c of str)
+		{
+			if(c == '0') continue;
+			acc += c.charCodeAt(0);
+		}
+
+		return acc;
+	}
+
+	const hard = exports.hardwareSerial(); 
+	
+	if(!hard) return hard; 
+	return hash(hard.slice(0,8)) + hard.slice(8);
+}
+
